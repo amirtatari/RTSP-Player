2593d6796991bc5739463c30465b4ea4e8d81442
Amirshayan1 <sh.tatari18@gmail.com>
2026-02-04 17:27:41 +0100

    feat: added logger
    available log lvls: debug, info, warning and error

--- utils/logger/logger.cpp
+++ utils/logger/logger.cpp
@@ -1,1 +1,35 @@
 #include "logger.hpp"
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+
+Logger& Logger::instance() 
+{
+    static Logger instance;
+    return instance;
+}
+
+void Logger::log(LogLevel level, std::string_view message) 
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    const auto now {std::chrono::system_clock::now()};
+    const auto time {std::chrono::system_clock::to_time_t(now)};
+    const auto ms {std::chrono::duration_cast<std::chrono::milliseconds>(
+        now.time_since_epoch()) % 1000};
+
+    std::ostream& out = (level == LogLevel::ERROR) ? std::cerr : std::cout;
+
+    out << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S")
+        << "." << std::setfill('0') << std::setw(3) << ms.count() << "] ";
+
+    switch (level) 
+    {
+        case LogLevel::DEBUG:   out << "[DEBUG] "; break;
+        case LogLevel::INFO:    out << "[INFO] "; break;
+        case LogLevel::WARNING: out << "[WARNING] "; break;
+        case LogLevel::ERROR:   out << "[ERROR] "; break;
+    }
+
+    out << message << '\n';
+}
--- utils/logger/logger.hpp
+++ utils/logger/logger.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <string_view>
+#include <mutex>
+
+#define logger Logger::instance()
+
+
+/// log levels for the application
+enum class LogLevel {DEBUG, INFO, WARNING, ERROR};
+
+/// singleton logger class
+class Logger 
+{
+public:
+    /// get the singleton instance
+    static Logger& instance();
+
+    /// log a message
+    void log(LogLevel level, std::string_view message);
+
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    Logger(Logger&&) = delete;
+    Logger& operator=(Logger&&) = delete;
+
+private:
+    Logger() = default;
+    ~Logger() = default;
+
+    std::mutex m_mutex;
+};
