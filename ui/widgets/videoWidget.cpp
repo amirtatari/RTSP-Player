1cf066d947f9709f4291110363d3da88a387d2b9
Amirshayan1 <sh.tatari18@gmail.com>
2026-02-05 18:07:44 +0100

    fix: use minimum size to fix the size of the widget

--- ui/widgets/videoWidget.cpp
+++ ui/widgets/videoWidget.cpp
@@ -1,7 +1,16 @@
 #include "videoWidget.hpp"
-#include <stdexcept>
 
-void VideoWidget::changeSize(int x, int y, int width, int height)
-{
-    setGeometry(QRect{x, y, width, height});
-}
+VideoWidget::VideoWidget(int x, int y, int width, int height, 
+                            const QString &name, QWidget *parentPtr)
+    : QWidget{parentPtr}
+    {
+        // allow the widget to be styled via stylesheets
+        setAttribute(Qt::WA_StyledBackground);
+
+        // set the size and location hints for the layout
+        setMinimumSize(width, height);
+        
+        // set the object name and change the background color to black
+        setObjectName(name);
+        setStyleSheet(QString("#%1 { background-color: black; }").arg(name));
+    }
--- ui/widgets/videoWidget.hpp
+++ ui/widgets/videoWidget.hpp
@@ -10,25 +10,11 @@ class VideoWidget : public QWidget
 
 public:
     // CTOR
-    explicit VideoWidget(int x, int y, int width, int height, const QString &name,
-                          QWidget *parentPtr = nullptr) noexcept
-        : QWidget{parentPtr}
-    {
-        // set the size and location of the widget
-        setGeometry(QRect{x, y, width, height});
-
-        // set the object name
-        setObjectName(name);
-
-        // set the color
-        setStyleSheet("background-color : black");
-    }
+    explicit VideoWidget(int x, int y, int width, int height, 
+                        const QString &name, QWidget *parentPtr = nullptr);
 
     VideoWidget(const VideoWidget &rhs) = delete;
     VideoWidget &operator=(const VideoWidget &rhs) = delete; 
     VideoWidget(VideoWidget &&rhs) = delete;                
     VideoWidget &operator=(VideoWidget &&rhs) = delete;      
-
-    // changes the size of the widget
-    void changeSize(int x, int y, int width, int height);
 };
