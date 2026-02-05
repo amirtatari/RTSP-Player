2b2bb6bc81e8ecc5664303222da5ae94af98e492
Amirshayan1 <sh.tatari18@gmail.com>
2026-02-05 17:39:26 +0100

    feat: added bus and bus callback to get debuging messages

--- backend/gstreamer/gstPlayer.cpp
+++ backend/gstreamer/gstPlayer.cpp
@@ -1,53 +1,86 @@
 #include "gstPlayer.hpp"
 
-void GstPlayer::stop() {
-  // return if the player element is nullptr
+#include <gst/video/videooverlay.h>
+
+GstPlayer::GstPlayer(QObject* parent) 
+  : QObject{parent} 
+  {}
+
+GstPlayer::~GstPlayer() 
+{ 
+  stop(); 
+}
+
+void GstPlayer::stop() 
+{
   if (m_playerElem == nullptr)
     return;
 
   // stop the player element set its state to null
-  gst_element_set_state(m_playerElem.get(), GST_STATE_PAUSED);
   gst_element_set_state(m_playerElem.get(), GST_STATE_NULL);
 
   // memory cleanup
+  if (m_bus)
+    gst_bus_remove_watch(m_bus.get());
+  
   m_playerElem.reset();
   m_bus.reset();
   m_error.reset();
   m_message.reset();
 }
 
-bool GstPlayer::playStream(const std::string& pipeline, unsigned int windowId) {
-  if (m_playerElem) {
-    return false;
-  }
+bool GstPlayer::playStream(const std::string& pipeline, unsigned int windowId) 
+{
+  if (m_playerElem)
+    stop();
 
   // check the input variables
-  if (pipeline.empty()) {
+  if (pipeline.empty()) 
+  {
+    emit errorOccurred("Empty pipeline description");
     return false;
   }
 
   // initialize the player element using the description
   GError* err{nullptr};
   m_playerElem.reset(gst_parse_launch(pipeline.c_str(), &err));
-  m_error.reset(err);
-
-  if (!m_playerElem) {
+  
+  if (err) 
+  {
+    const QString errorMsg {QString("Failed to parse pipeline: %1").arg(err->message)};
+    emit errorOccurred(errorMsg);
+    g_error_free(err);
     return false;
   }
+
+  if (!m_playerElem) 
+  {
+    emit errorOccurred("Failed to create pipeline (unknown error)");
+    return false;
+  }
+
+  // setup bus watch
+  m_bus.reset(gst_element_get_bus(m_playerElem.get()));
+  gst_bus_add_watch(m_bus.get(), busCallback, this);
 
   // get the sink element & set video overlay
   std::unique_ptr<GstElement, GstObjectDeleter> videoSink{
       gst_bin_get_by_interface(GST_BIN(m_playerElem.get()), GST_TYPE_VIDEO_OVERLAY)};
 
-  if (videoSink) {
+  if (videoSink)
     gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(videoSink.get()), windowId);
+  else 
+  {
+    emit errorOccurred("Could not find video sink with overlay support");
+    return false;
   }
 
   // set player element state to playing
   GstStateChangeReturn ret{gst_element_set_state(m_playerElem.get(), GST_STATE_PLAYING)};
 
-  if (ret == GST_STATE_CHANGE_FAILURE) {
-    stop();
+  if (ret == GST_STATE_CHANGE_FAILURE) 
+  {
+    emit errorOccurred("Failed to set pipeline to PLAYING state");
     return false;
   }
 
@@ -57,4 +90,55 @@ bool GstPlayer::playStream(const std::string& pipeline, unsigned int windowId) {
 bool GstPlayer::init()
 {
   return gst_init_check(nullptr, nullptr, nullptr);
+}
+
+gboolean GstPlayer::busCallback(GstBus* bus, GstMessage* msg, gpointer data)
+{
+  auto* self {static_cast<GstPlayer*>(data)};
+  
+  switch (GST_MESSAGE_TYPE(msg)) {
+    case GST_MESSAGE_ERROR: 
+    {
+      GError* err {nullptr};
+      gchar* debug {nullptr};
+      gst_message_parse_error(msg, &err, &debug);
+      const QString errorMsg {QString("GStreamer error from %1: %2")
+                             .arg(GST_OBJECT_NAME(msg->src))
+                             .arg(err->message)};
+      emit self->errorOccurred(errorMsg);
+      g_error_free(err);
+      g_free(debug);
+      break;
+    }
+    case GST_MESSAGE_WARNING: 
+    {
+      GError* err {nullptr};
+      gchar* debug {nullptr};
+      gst_message_parse_warning(msg, &err, &debug);
+      const QString warningMsg {QString("GStreamer warning from %1: %2")
+                             .arg(GST_OBJECT_NAME(msg->src))
+                             .arg(err->message)};
+      emit self->errorOccurred(warningMsg);
+      g_error_free(err);
+      g_free(debug);
+      break;
+    }
+    case GST_MESSAGE_EOS:
+      emit self->eosReached();
+      break;
+    case GST_MESSAGE_STATE_CHANGED: 
+    {
+      if (GST_MESSAGE_SRC(msg) == GST_OBJECT(self->m_playerElem.get())) 
+      {
+        GstState oldState, newState, pendingState;
+        gst_message_parse_state_changed(msg, &oldState, &newState, &pendingState);
+        emit self->stateChanged(QString::fromStdString(gst_element_state_get_name(newState)));
+      }
+      break;
+    }
+    default:
+      break;
+  }
+  
+  return TRUE;
 }
--- backend/gstreamer/gstPlayer.hpp
+++ backend/gstreamer/gstPlayer.hpp
@@ -4,37 +4,57 @@
 #include <memory>
 #include <gst/gst.h>
 #include <gst/video/videooverlay.h>
+#include <QObject>
+#include <QString>
 
-struct GstObjectDeleter {
-  void operator()(gpointer ptr) const {
+struct GstObjectDeleter 
+{
+  void operator()(gpointer ptr) const 
+  {
     if (ptr)
       gst_object_unref(ptr);
   }
 };
 
-struct GErrorDeleter {
-  void operator()(GError* ptr) const {
+struct GErrorDeleter 
+{
+  void operator()(GError* ptr) const 
+  {
     if (ptr)
       g_error_free(ptr);
   }
 };
 
-struct GstMessageDeleter {
-  void operator()(GstMessage* ptr) const {
+struct GstMessageDeleter 
+{
+  void operator()(GstMessage* ptr) const 
+  {
     if (ptr)
       gst_message_unref(ptr);
   }
 };
 
-class GstPlayer
+class GstPlayer : public QObject
 {
+  Q_OBJECT
+
   std::unique_ptr<GstElement, GstObjectDeleter> m_playerElem;
   std::unique_ptr<GstBus, GstObjectDeleter> m_bus;
   std::unique_ptr<GstMessage, GstMessageDeleter> m_message;
   std::unique_ptr<GError, GErrorDeleter> m_error;
 
+  static gboolean busCallback(GstBus* bus, GstMessage* msg, gpointer data);
+
 public:
+  explicit GstPlayer(QObject* parent = nullptr);
+  ~GstPlayer();
+
   bool playStream(const std::string& pipeline, unsigned int windowId);
   void stop();
   bool init();
+
+signals:
+  void errorOccurred(const QString& message);
+  void stateChanged(const QString& state);
+  void eosReached();
 };
